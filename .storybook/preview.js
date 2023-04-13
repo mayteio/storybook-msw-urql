df4257e6b5b93a35139ea52a6d35ab9f81499b2c
Harley Alexander <h@pachama.com>
2023-04-14 09:05:37 +1000

    chore: introduce failing tooling

---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+initialize();
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  decorators: [mswDecorator],
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
