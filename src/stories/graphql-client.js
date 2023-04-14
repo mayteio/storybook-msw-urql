05cc3809b6be7f6bf1e0694d3135a9e91ce8e28e
Tomoya Kashifuku <tnyo43@gmail.com>
2023-04-15 02:41:36 +0900

    overwrite request header to let msw intercept the urql requst.

--- src/stories/graphql-client.js
+++ src/stories/graphql-client.js
@@ -3,4 +3,9 @@ import { createClient, fetchExchange } from "urql";
 export const graphqlClient = createClient({
   url: "http://localhost/graphql",
   exchanges: [fetchExchange],
+  fetchOptions: {
+    headers: {
+      accept: "*/*"
+    }
+  }
 });
